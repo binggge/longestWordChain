8ae597ebbc3fb6b625a3f37780cd1753950695cc
binggge <15231112@buaa.edu.cn>
2019-03-03 22:15:16 +0800

    define interface

--- Wordlist/Wordlist/Core.cpp
+++ Wordlist/Wordlist/Core.cpp
@@ -1,5 +1,5 @@
 #include "Core.h"
-
+#include <iostream>
 
 
 Core::Core()
@@ -9,4 +9,14 @@ Core::Core()
 
 Core::~Core()
 {
+}
+
+int Core::gen_chain_word(char * words[], int len, char * result[], char head, char tail, bool enable_loop)
+{
+	return 0;
+}
+
+int Core::gen_chain_char(char * words[], int len, char * result[], char head, char tail, bool enable_loop)
+{
+	return 0;
 }
--- Wordlist/Wordlist/Core.h
+++ Wordlist/Wordlist/Core.h
@@ -4,5 +4,7 @@ class Core
 public:
 	Core();
 	~Core();
+	int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
+	int gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
 };
 
