f6126fe008e29dcd64a4c563b81c3e3f91cda05d
canuse <liujc623@gmail.com>
2019-03-04 19:28:50 +0800

    build map for bfs

--- Wordlist/Wordlist/Core.cpp
+++ Wordlist/Wordlist/Core.cpp
@@ -11,8 +11,52 @@ Core::~Core()
 {
 }
 
+struct wordnode {
+	char startChar;
+	char endChar;
+	char *word;
+	int distanceToTail;
+	bool selfLoop;
+	char *path;
+} wordNode[1000];
+
+int createMap(char *words[], int len) {
+	// create an acyclic map
+	int pos = 0;
+	for (int i = 0; i < len; i++) {
+		char *currentWord;
+		char startChar, endChar;
+		currentWord = words[i];
+		startChar = currentWord[0];
+		endChar = currentWord[strlen(currentWord) - 1];
+		bool flag = false;
+		for (int j = 0; j < pos; j++) {
+			if (wordNode[j].startChar == startChar && wordNode[j].endChar == endChar) {
+				flag = true;
+				break;
+			}
+		}
+		if (!flag) {
+			// new node
+			wordNode[pos].startChar = startChar;
+			wordNode[pos].endChar = endChar;
+			wordNode[pos].word = currentWord;
+			wordNode[pos].distanceToTail = 0;
+			wordNode[pos].selfLoop = false;
+			if (startChar == endChar) wordNode[pos].selfLoop = true;
+			pos++;
+		}
+	}
+	return pos;
+}
+
 int Core::gen_chain_word(char * words[], int len, char * result[], char head, char tail, bool enable_loop)
 {
+	int wnLen = 0;
+	if (!enable_loop) {
+		// no loop
+		wnLen = createMap(words, len);
+	}
 	return 0;
 }
 
