b505f3344586b2880614e1e99483c502dce7bd5c
canuse <liujc623@gmail.com>
2019-03-04 19:45:57 +0800

    find tails for bfs

--- Wordlist/Wordlist/Core.cpp
+++ Wordlist/Wordlist/Core.cpp
@@ -50,12 +50,44 @@ int createMap(char *words[], int len) {
 	return pos;
 }
 
+void getTails(int wnLen, char *retTails) {
+	int alpList[26];
+	for (int &i : alpList) {
+		i = -10000000;
+	}
+	for (int i = 0; i < wnLen; i++) {
+		alpList[wordNode[i].endChar - 'a'] = 0;
+
+	}
+	for (int i = 0; i < wnLen; i++) {
+		if (!wordNode[i].selfLoop) {
+			alpList[wordNode[i].startChar - 'a'] += 1;
+		}
+	}
+	int pos = 0;
+	for (int i = 0; i < 26; i++) {
+		if (alpList[i] == 0) {
+			retTails[pos] = (char)(i + 'a');
+			pos++;
+		}
+	}
+	retTails[pos] = '\0';
+}
+
 int Core::gen_chain_word(char * words[], int len, char * result[], char head, char tail, bool enable_loop)
 {
 	int wnLen = 0;
 	if (!enable_loop) {
 		// no loop
 		wnLen = createMap(words, len);
+	}
+	char resultTails[26];
+	if (tail == 0) {
+		getTails(wnLen, resultTails);
+	}
+	else {
+		resultTails[0] = tail;
+		resultTails[1] = '\0';
 	}
 	return 0;
 }
