343aecdc014f0a17248991faeaf1ec25e01e6183
canuse <liujc623@gmail.com>
2019-03-04 22:26:54 +0800

    fix bugs, finish bfs for -r==false

--- Wordlist/Wordlist/Core.h
+++ Wordlist/Wordlist/Core.h
@@ -4,6 +4,26 @@ class Core
 public:
 	Core();
 	~Core();
+	struct wordnode {
+		char startChar;
+		char endChar;
+		char *word;
+		bool selfLoop;
+	} wordNode[1000];
+
+	struct charnode {
+		char endChar;
+		int reachableChar[26];
+		bool selfLoop;
+		int distanceToTail = 0;
+	} charNode[30];
+	struct queueitem {
+		char itemChar;
+		int currDist;
+	} bfsQueue[100000];
+	int createMap(char *words[], int len);
+	void getTails(int wnLen, char *retTails);
+	void bfs_gcw_no_r(char startTail);
 	int gen_chain_word(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
 	int gen_chain_char(char* words[], int len, char* result[], char head, char tail, bool enable_loop);
 };
